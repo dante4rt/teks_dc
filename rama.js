c3440b9338d62c0fa77d45844f017003fb3804d8
Muhammad Ramadhani <rxmxdhxni@gmail.com>
2022-02-16 15:49:42 +0800

    Add files via upload

---
+++ rama.js
@@ -0,0 +1,333 @@
+[
+    {
+        "by": "A. France",
+        "quote": "Soon Rich ser"
+    },
+    {
+        "by": "Abraham Lincoln",
+        "quote": "Keep Fighting guys"
+    },
+    {
+        "by": "Aeschylus",
+        "quote": "Hahahahaha"
+    },
+    {
+        "by": "Aesop",
+        "quote": "Morning guys"
+    },
+    {
+        "by": "Albert Einstein",
+        "quote": "How are you"
+    },
+    {
+        "by": "Albert Einstein",
+        "quote": "Lets fucking gooo, and stay with me"
+    },
+    {
+        "by": "Albert Einstein",
+        "quote": "Damn thats very crazy"
+    },
+    {
+        "by": "Albert Einstein",
+        "quote": "You are so funny hahaha LOL"
+    },
+    {
+        "by": "Albert Enstein",
+        "quote": "No Spam guys"
+    },
+    {
+        "by": "Alex Osborn",
+        "quote": "Keep going on"
+    },
+    {
+        "by": "Alexander A. Bogomoletz",
+        "quote": "Lets gooooo gogogoogo"
+    },
+    {
+        "by": "Alexander Solzhenitsyn",
+        "quote": "i always in here be with you"
+    },
+    {
+        "by": "Ali Javan",
+        "quote": "So fvcking impresif"
+    },
+    {
+        "by": "Ali bin Abi Thalib",
+        "quote": "Hello every one"
+    },
+    {
+        "by": "Ali bin Abi Thalib",
+        "quote": "Im ready for it"
+    },
+    {
+        "by": "Ali bin Abi Thalib",
+        "quote": "Wen WL ser?"
+    },
+    {
+        "by": "Anne M. Lindbergh",
+        "quote": "I need WL"
+    },
+    {
+        "by": "Anonim",
+        "quote": "i want to be rich haha"
+    },
+    {
+        "by": "Anonim",
+        "quote": "this is crazy community"
+    },
+    {
+        "by": "Antonie De Saint",
+        "quote": "I love you guys"
+    },
+    {
+        "by": "Aristoteles",
+        "quote": "we can doit bro, work hard together hahah to get her not together"
+    },
+    {
+        "by": "Arnold Glasow",
+        "quote": "HAHAHAHAHAHA"
+    },
+    {
+        "by": "Art Buchwald",
+        "quote": "Keep active guys"
+    },
+    {
+        "by": "Artemus Ward",
+        "quote": "dont forget to take a rest"
+    },
+    {
+        "by": "Ashleigh Brilliant",
+        "quote": "HAHAHAHAHA Lmao"
+    },
+    {
+        "by": "Augustine",
+        "quote": "Dont be SPAM"
+    },
+    {
+        "by": "Ayn Rand",
+        "quote": "Whitelist soon haha"
+    },
+    {
+        "by": "B. J. Habibie",
+        "quote": "WIbu"
+    },
+    {
+        "by": "Balzac",
+        "quote": "helo helo helo haha"
+    },
+    {
+        "by": "Budha Gautama",
+        "quote": "ween wl seer?"
+    },
+    {
+        "by": "Bunda Teresa",
+        "quote": "here weee goo"
+    },
+    {
+        "by": "Bunda Teresa",
+        "quote": "Where are you from?"
+    },
+    {
+        "by": "Bung Hatta",
+        "quote": "thats so cool"
+    },
+    {
+        "by": "Burn",
+        "quote": "Calm down guys"
+    },
+    {
+        "by": "Carol Burnet",
+        "quote": "lets do this"
+    },
+    {
+        "by": "Charles Darwin",
+        "quote": "Wen LVL 10 ser"
+    },
+    {
+        "by": "Charles R. Swindoll",
+        "quote": "How are you today?"
+    },
+    {
+        "by": "Ching Hai",
+        "quote": "Lets go baby"
+    },
+    {
+        "by": "Ching Hai",
+        "quote": "Good day for everyone"
+    },
+    {
+        "by": "Ching Hai",
+        "quote": "So tired"
+    },
+    {
+        "by": "Ching Hai",
+        "quote": "Neeed wl"
+    },
+    {
+        "by": "Cowper",
+        "quote": "You are so cool guys"
+    },
+    {
+        "by": "Christopher Colombus",
+        "quote": "mpsshhh ah HAHAHAHa"
+    },
+    {
+        "by": "Cicero",
+        "quote": "aighh"
+    },
+    {
+        "by": "Cicero",
+        "quote": "Be patient ser we can do it"
+    },
+    {
+        "by": "Clarence Darrow",
+        "quote": "LOL"
+    },
+    {
+        "by": "Confucius",
+        "quote": "Keep going on"
+    },
+    {
+        "by": "Confucius",
+        "quote": "WaGMI"
+    },
+    {
+        "by": "Confucius",
+        "quote": "Nice "
+    },
+    {
+        "by": "Confucius",
+        "quote": "Wen Moon guys?"
+    },
+    {
+        "by": "John Wolfgang",
+        "quote": "LFG"
+    },
+    {
+        "by": "Joseph Addison",
+        "quote": "Dont be Cry Guys"
+    },
+    {
+        "by": "Julia Roberts",
+        "quote": "Keep Fighting"
+    },
+    {
+        "by": "Junius",
+        "quote": "LMAO"
+    },
+    {
+        "by": "Kahlil Gibran",
+        "quote": "Hahahaha LOL"
+    },
+    {
+        "by": "1",
+        "quote": "Wait until your level goes up and keep active guys"
+    },
+    {
+        "by": "2",
+        "quote": "hope you enjoy your day mates"
+    },
+    {
+        "by": "3",
+        "quote": "looking through twit i founwe will make it everyone! <3d this project"
+    },
+    {
+        "by": "4",
+        "quote": "this projeclfg!this project will definitely be in the top"
+    },
+    {
+        "by": "5",
+        "quote": "Any update?"
+    },
+    {
+        "by": "6",
+        "quote": "how to get?"
+    },
+    {
+        "by": "7",
+        "quote": "I'm horny"
+    },
+    {
+        "by": "8",
+        "quote": "I'm happy to be here"
+    },
+    {
+        "by": "9",
+        "quote": "Keep spirit"
+    },
+    {
+        "by": "10",
+        "quote": "Pick me up Daddy"
+    },
+    {
+        "by": "11",
+        "quote": "Hope I can be part of this awesome project"
+    },
+    {
+        "by": "12",
+        "quote": "Guys, don't miss this opportunity, this project will be a great investment for you"
+    },
+    {
+        "by": "13",
+        "quote": "I am very enthusiastic because this is an excellent project, let's take it to the moon"
+    },
+    {
+        "by": "14",
+        "quote": "A very promising project to work on"
+    },
+    {
+        "by": "15",
+        "quote": "LFG guys!! keep it hype!"
+    },
+    {
+        "by": "16",
+        "quote": "Impresive and great project"
+    },
+    {
+        "by": "17",
+        "quote": "Keep Pushing level XD. but i believe this project will success. lets pushing together guys"
+    },
+    {
+        "by": "18",
+        "quote": "Dont Forget to invite your friend and grow this project together!"
+    },
+    {
+        "by": "19",
+        "quote": "I hope more friends will participate in the understand"
+    },
+    {
+        "by": "20",
+        "quote": "I Think, this is very strong project"
+    },
+    {
+        "by": "21",
+        "quote": "With such an enthusiastic and excellent team"
+    },
+	{ "quote" :   
+	"This project looks very interesting. I am interested and I will support this project until it is successful according to the plan that has been set"
+	},
+{ "quote" :   "Guys don't miss this opportunity this project will be a great investment for you."},
+{ "quote" :   "I am very enthusiastic because this is an excellent project let's take it to the moon."},
+{ "quote" :   "I believe this is a faithful project.The projector has a lot of attractions so hopefully the project will be better in the future and will be the best."},
+{ "quote" :   "I Think this is very strong project."},
+{ "quote" :   "This is definitely going to be HUGE!"},
+{ "quote" :   "I am so glad to be part of this."},
+{ "quote" :   "I am supporting it. always success for the development team to the moon"},
+{ "quote" :   "Nice project and I look forward to the growth and success of this project"},
+{ "quote" :   "I hope more friends will participate in the understand"},
+{ "quote" :   "With such an enthusiastic and excellent team"},
+{ "quote" :   "Impresive and great project."},
+{ "quote" :   "I trust this project can be successfully"},
+{ "quote" :   "LFG guys!! keep it hype!"},
+{ "quote" :   "Dont Forget to invite your friend and grow this project together!"},
+{ "quote" :   "Let's make this project more hype! we strong community!"},
+{ "quote" :   "Keep Pushing level XD. but i believe this project will success. lets pushing together guys :)"},
+{ "quote" :   "Dont Forget to invite your friend and grow this project together!"},
+{ "quote" :   "Great project guys and don't forget share this project to your friends"},
+{ "quote" :   "Let's share this social media project to grow."},
+{ "quote" :   "Keep supporting this project until the end."},
+{ "quote" :   "keep active guys"},
+{ "quote" :   "A very promising project to work on."},
+{ "quote" :   "Don't forget to invite your friends into this project so that the project is getting more and more people who know it."},
+{ "quote" :   "I hope you have a good day every day and don't forget to continue supporting this project."}
+]
